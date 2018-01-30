321a14678c4942a85ad7939be576f36e41f7497d
williamfelippe <wfelippesilva@gmail.com>
2018-01-30 01:29:42 -0200

    feat: Changing sentence in quiz screen

--- scenes/Quiz.js
+++ scenes/Quiz.js
@@ -146,7 +146,7 @@ const QuestionResults = ({
         <Container>
             <QuizView>
                 <QuestionResultTitle>
-                    Results here =)
+                    Correct answers:
                 </QuestionResultTitle>
 
                 <QuestionResultScore>
