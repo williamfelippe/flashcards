3ca3765883c18245f58b9010a6964ff47d121dc8
William Silva <william.silva@zup.com.br>
2018-01-30 15:20:46 -0200

    refactor: Exporting strings to specific files

---
+++ strings/errors.js
@@ -0,0 +1,2 @@
+export const cantBeEmptyError = (element) => `${element} can't be empty`
+export const titleCantBeEmptyError = `Title can't be empty`
--- strings/labels.js
+++ strings/labels.js
@@ -1,3 +1,9 @@
 export const noDecksAtTheMomentLabel = 'No decks at the moment'
 export const noQuestionsHereLabel = 'No questions here'
 export const correctAnswersLabel = 'Correct answers'
+export const noCardsAtTheMomentLabel = 'No cards at the moment'
+export const amountOfCardsLabel = (amount) => amount === 1 ? `${amount} card` : `${amount} cards`
+export const notificationBodyLabel = 'Do not forget to study today!'
+export const questionLabel = 'Question'
+export const answerLabel = 'Answer'
+export const titleOfDeckLabel = 'Deck title'
